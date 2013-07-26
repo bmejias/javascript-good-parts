e4e3abf38a2e3718dc0a1f84f25696f7e6647608
Boriss Mejias <tchorix@gmail.com>
2013-07-26 09:37:32 +0200

    adding exercise on factorial with tail recursion

--- hanoi.js
+++ hanoi.js
@@ -1,3 +1,5 @@
+// The Towers of Hanoi
+
 var hanoi = function (discs, src, aux, dest) {
     if (discs > 0) {
         hanoi (discs - 1, src, dest, aux);
@@ -12,3 +14,17 @@ hanoi(2, 'Src', 'Aux', 'Dest');
 console.log("-------------------------");
 console.log(" Hanoi 3");
 hanoi(3, 'Src', 'Aux', 'Dest');
+
+// Tail Recursion
+
+function factorial(i, a) {
+    a = a || 1;
+    if (i < 2) {
+        return a;
+    }
+    return factorial(i - 1, i * a);
+}
+
+for (var i = 0; i < 12; i +=1) {
+    console.log(i + "! = " + factorial(i));
+}
