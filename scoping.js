1ba4c7d5e6991f7f2c4372c0723b7e042ad4d141
Boriss Mejias <tchorix@gmail.com>
2013-07-30 08:38:29 +0200

    understanding scoping, and adding a small code that when commented out allows me to run the code with RhinoScript

--- scoping.js
+++ scoping.js
@@ -1,3 +1,13 @@
+// Rhino and Nodejs produce exactly the same result, so scoping is handled by
+// both in the same way. 
+//
+// Code for Rhino - Convert console.log into a print
+// var console = {
+//     log: function(msg) {
+//         print(msg);
+//     }
+// };
+
 // Bad reference to variable i, where we are interested in the value of the
 // variable at creation time of the function
 
@@ -37,3 +47,26 @@ add_the_handlers(nodes_ii);
 for (var node in nodes_ii) {
     nodes_ii[node].onclick("event");
 }
+
+// Code from page 36
+
+var foo = function () {
+    var a = 3;
+    var b = 5;
+    var bar = function () {
+        var b = 7;
+        var c = 11;
+        console.log("bar b: " + b);  // 7
+        console.log("bar c: " + c);  // 11
+        a += b + c;
+        console.log("bar a: " + a);  // 21
+    };
+
+    console.log("foo a: " + a);  // 3
+    console.log("foo b: " + b);  // 5
+    bar();
+    console.log("after bar() a: " + a);  // 3 !? wrong! this is 21
+    console.log("after bar() b: " + b);  // 5
+};
+
+foo();
